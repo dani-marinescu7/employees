0edd729ea67444d580118e093a3a24fe75d02676
Daniel <dani.marinescu7@gmail.com>
2024-02-15 20:58:02 +0200

    Added routes

---
+++ server/routes/authApi.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { catchErrors } = require("../handlers/errorHandlers");
+const {
+  isValidToken,
+  login,
+  logout,
+} = require("../controllers/authController");
+
+router.route("/login").post(catchErrors(login));
+
+router.route("/login").post(catchErrors(login));
+router.route("/logout").post(isValidToken, catchErrors(logout));
+
+module.exports = router;
