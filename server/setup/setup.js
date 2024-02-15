276116aff1caab2bb548191c1f4ca70f11c1814b
Daniel <dani.marinescu7@gmail.com>
2024-02-15 21:05:41 +0200

    Added setup file

---
+++ server/setup/setup.js
@@ -0,0 +1,28 @@
+require("dotenv").config({ path: __dirname + "/../.env" });
+const fs = require("fs");
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+
+async function createAdmin() {
+  try {
+    const Admin = require("../models/Admin");
+    var newAdmin = new Admin();
+    const passwordHash = newAdmin.generateHash("123456");
+
+    await new Admin({
+      email: "admin@demo.com",
+      password: passwordHash,
+      name: "admin",
+      surname: "demo",
+    }).save();
+    console.log("👍👍👍👍👍👍👍👍 Admin created : Done!");
+    process.exit();
+  } catch (e) {
+    console.log("\n👎👎👎👎👎👎👎👎 Error! The Error info is below");
+    console.log(e);
+    process.exit();
+  }
+}
+createAdmin();
