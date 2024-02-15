6d6dcb3c4dffb03300de690d26c2945ef1135e87
Daniel <dani.marinescu7@gmail.com>
2024-02-15 19:36:01 +0200

    Added theme

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+
+
+function App() {
+    const mode = useSelector((state) => state.global.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Routes>
+
+                    </Routes>
+                </ThemeProvider>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,97 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
-
-import Layout from "./Pages/Layout";
-import ErrorPage from "./Pages/ErrorPage";
-import EmployeeList from "./Pages/EmployeeList";
-import EmployeeSearchList from "./Pages/EmployeeSearchList";
-import EmployeeMissingList from "./Pages/EmployeeMissingList"
-import EmployeeCreator from "./Pages/EmployeeCreator";
-import EmployeeUpdater from "./Pages/EmployeeUpdater";
-import CompanyCreator from "./Pages/CompanyCreator";
-
-import EquipmentLayout from "./Pages/EquipmentLayout";
-import EquipmentList from "./Pages/EquipmentList";
-import EquipmentCreator from "./Pages/EquipmentCreator";
-import EquipmentUpdater from "./Pages/EquipmentUpdater";
-
 import "./index.css";
-import TableTest from "./Pages/TableTest";
-import FormTest from "./Pages/FormTest";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "state/api";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+    [api.reducerPath]: api.reducer,
   },
-  {
-    path: "/",
-    element: <EmployeeList />,
-  },
-  {
-    path: "/:column/:order",
-    element: <EmployeeList />,
-  },
-  {
-    path: "/employees/:search",
-    element: <EmployeeSearchList />,
-  },
-  {
-    path: "/missing",
-    element: <EmployeeMissingList />,
-  },
-  {
-    path: "/create",
-    element: <EmployeeCreator />,
-  },
-  {
-    path: "/create-company",
-    element: <CompanyCreator />,
-  },
-  {
-    path: "/update/:id",
-    element: <EmployeeUpdater />,
-  },
-  {
-    path: "/table-test",
-    element: <TableTest />,
-  },
-  {
-    path: "/form-test",
-    element: <FormTest />,
-  },
-  {
-    path: "/equipments",
-    element: <EquipmentLayout />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/equipments",
-    element: <EquipmentList />,
-  },
-  {
-    path: "/create-equipment",
-    element: <EquipmentCreator />,
-  },
-  {
-    path: "/update-equipment/:id",
-    element: <EquipmentUpdater />,
-  },
-]);
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router}>
-      <Route />
-    </RouterProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
